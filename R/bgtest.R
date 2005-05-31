ed6c938ef9c5ba29ad1fe06267cb6f3e4cbd203b
Achim Zeileis <Achim.Zeileis@R-project.org>
2005-05-31 00:00:00 +0000

    version 0.9-12

---
+++ R/bgtest.R
@@ -0,0 +1,52 @@
+bgtest <- function(formula, order = 1, type = c("Chisq", "F"), data = list())
+{
+  dname <- paste(deparse(substitute(formula)))
+
+  if(!inherits(formula, "formula")) {
+    X <- if(is.matrix(formula$x))
+           formula$x
+         else model.matrix(terms(formula), model.frame(formula))
+    y <- if(is.vector(formula$y))
+           formula$y
+         else model.response(model.frame(formula))
+  } else {
+    mf <- model.frame(formula, data = data)
+    y <- model.response(mf)
+    X <- model.matrix(formula, data = data)
+  }  
+
+  n <- nrow(X)
+  k <- ncol(X)
+  order <- 1:order
+  m <- length(order)
+  resi <- lm.fit(X,y)$residuals
+
+  Z <- sapply(order, function(x) c(rep(0, x), resi[1:(n-x)]))
+  auxfit <- lm.fit(cbind(X,Z), resi)
+
+  switch(match.arg(type),
+
+  "Chisq" = {
+    bg <- n * sum(auxfit$fitted^2)/sum(resi^2)
+    p.val <- pchisq(bg, m, lower.tail = FALSE)
+    df <- m
+    names(df) <- "df"
+  },
+
+  "F" = {
+    uresi <- auxfit$residuals
+    rresi <- lm.fit(X,resi)$residuals
+    bg <- ((sum(rresi^2) - sum(uresi^2))/m) / (sum(uresi^2) / (n-k-m))
+    p.val <- pf(bg, df1 = m, df2 = n-k, lower.tail = FALSE)
+    df <- c(m, n-k)
+    names(df) <- c("df1", "df2")
+  })
+
+  names(bg) <- "LM test"
+  RVAL <- list(statistic = bg, parameter = df,
+               method = paste("Breusch-Godfrey test for serial correlation of order", max(order)),
+               p.value = p.val,
+               data.name =   dname)
+  class(RVAL) <- "htest"
+  return(RVAL)
+}
---
+++ R/dwtest.R
@@ -0,0 +1,76 @@
+dwtest <- function(formula, alternative = c("greater", "two.sided", "less"),
+  iterations = 15, exact = NULL, tol = 1e-10, data = list())
+{
+  dname <- paste(deparse(substitute(formula)))
+  alternative <- match.arg(alternative)
+
+  if(!inherits(formula, "formula")) {
+    X <- if(is.matrix(formula$x))
+           formula$x
+         else model.matrix(terms(formula), model.frame(formula))
+    y <- if(is.vector(formula$y))
+           formula$y
+         else model.response(model.frame(formula))
+  } else {
+    mf <- model.frame(formula, data = data)
+    y <- model.response(mf)
+    X <- model.matrix(formula, data = data)
+  }  
+   
+  n <- nrow(X)
+  if(is.null(exact)) exact <- (n < 100)
+  k <- ncol(X)
+  res <- lm.fit(X,y)$residuals
+  dw <- sum(diff(res)^2)/sum(res^2)
+  Q1 <- chol2inv(qr.R(qr(X)))
+  if(exact)
+  {
+    A <- diag(c(1,rep(2, n-2), 1))
+    A[abs(row(A)-col(A))==1] <- -1
+    MA <- diag(rep(1,n)) - X %*% Q1 %*% t(X)
+    MA <- MA %*% A
+    ev <- eigen(MA)$values[1:(n-k)]
+    if(any(Im(ev)>tol)) warning("imaginary parts of eigenvalues discarded")
+    ev <- Re(ev)
+    ev <- ev[ev>tol]
+    pdw <- function(dw) .Fortran("pan", as.double(c(dw,ev)), as.integer(length(ev)),
+             as.double(0), as.integer(iterations), x=double(1), PACKAGE = "lmtest")$x
+    pval <- switch(alternative,
+      "two.sided" = (2*min(pdw(dw), 1-pdw(dw))),
+      "less" = (1 - pdw(dw)),
+      "greater" = pdw(dw))
+
+    if((pval > 1) | (pval < 0))
+    {
+      warning("exact p value cannot be computed (not in [0,1]), approximate p value will be used")
+      exact <- FALSE
+    }
+  }
+  if(!exact)
+  {
+    AX <- matrix(as.vector(filter(X, c(-1, 2, -1))), ncol = k)
+    AX[1,] <- X[1,] - X[2,]
+    AX[n,] <- X[n,] - X[(n-1),]
+    XAXQ <- t(X) %*% AX %*% Q1
+    P <- 2*(n-1) - sum(diag(XAXQ))
+    Q <- 2*(3*n - 4) - 2* sum(diag(crossprod(AX) %*% Q1)) + sum(diag(XAXQ %*% XAXQ))
+    dmean <- P/(n-k)
+    dvar <- 2/((n-k)*(n-k+2)) * (Q - P*dmean)
+    pval <- switch(alternative,
+      "two.sided" = (2*pnorm(abs(dw-dmean), sd=sqrt(dvar), lower.tail = FALSE)),
+      "less" = pnorm(dw, mean = dmean, sd = sqrt(dvar), lower.tail = FALSE),
+      "greater" = pnorm(dw, mean = dmean, sd = sqrt(dvar)))
+  }
+
+  alternative <- switch(alternative,
+    "two.sided" = "true autocorelation is not 0",
+    "less" = "true autocorrelation is less than 0",
+    "greater" = "true autocorrelation is greater than 0")
+
+  names(dw) <- "DW"
+  RVAL <- list(statistic = dw, method = "Durbin-Watson test",
+    alternative = alternative, p.value= pval, data.name=dname)
+  class(RVAL) <- "htest"
+  return(RVAL)
+}
+
